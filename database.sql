0297b9a490d463d48e14cea4cba4478e794f56bd
Nicolas Mueller <nicolas.mueller2@gmail.com>
2023-02-13 09:17:10 +0100

    add database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 19. Dez 2022 um 20:18
+-- Erstellungszeit: 13. Feb 2023 um 08:16
 -- Server-Version: 8.0.31
 -- PHP-Version: 7.4.32
 
@@ -35,6 +35,37 @@ CREATE TABLE `categories` (
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `clubs`
+--
+
+CREATE TABLE `clubs` (
+  `id` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `clubs`
+--
+
+INSERT INTO `clubs` (`id`, `name`) VALUES
+(1, 'ol norska'),
+(2, 'OLG Oberwil'),
+(3, 'OLG Herzogenbuchsee'),
+(4, 'OLG Hondrich'),
+(5, 'ol.biel.seeland'),
+(6, 'OLG Thun'),
+(7, 'OL Regio Burgdorf'),
+(8, 'OLG Bern'),
+(9, 'Bucheggberger OL'),
+(10, 'OLG Biberist SO'),
+(11, 'OLV Langenthal'),
+(12, 'OLC Omström Sense'),
+(13, 'OLG Huttwil'),
+(14, 'Ol Regio Olten');
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `cups`
 --
 
@@ -58,6 +89,17 @@ CREATE TABLE `cups_categories` (
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `cups_clubs`
+--
+
+CREATE TABLE `cups_clubs` (
+  `clubId` int NOT NULL,
+  `cupId` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `races`
 --
 
@@ -66,7 +108,9 @@ CREATE TABLE `races` (
   `club` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `date` date NOT NULL,
   `solv_id` int NOT NULL,
-  `cupId` int NOT NULL
+  `cupId` int NOT NULL,
+  `calculation` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'NACHWUCHSCUP_STANDARD',
+  `id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,7 +122,9 @@ CREATE TABLE `races` (
 CREATE TABLE `results` (
   `runnerId` int NOT NULL,
   `raceId` int NOT NULL,
-  `points` smallint NOT NULL
+  `points` smallint NOT NULL,
+  `striked` tinyint(1) NOT NULL DEFAULT '0',
+  `cupId` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -108,6 +154,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indizes für die Tabelle `clubs`
+--
+ALTER TABLE `clubs`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- Indizes für die Tabelle `cups`
 --
 ALTER TABLE `cups`
@@ -120,9 +172,17 @@ ALTER TABLE `cups_categories`
   ADD KEY `cups_categories_ibfk_1` (`cup_id`);
 
 --
+-- Indizes für die Tabelle `cups_clubs`
+--
+ALTER TABLE `cups_clubs`
+  ADD KEY `cupId` (`cupId`),
+  ADD KEY `cups_clubs_ibfk_2` (`clubId`);
+
+--
 -- Indizes für die Tabelle `races`
 --
 ALTER TABLE `races`
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `solv_id_2` (`solv_id`),
   ADD KEY `cupId` (`cupId`);
 
@@ -130,8 +190,9 @@ ALTER TABLE `races`
 -- Indizes für die Tabelle `results`
 --
 ALTER TABLE `results`
-  ADD KEY `results_ibfk_2` (`runnerId`),
-  ADD KEY `results_ibfk_3` (`raceId`);
+  ADD PRIMARY KEY (`runnerId`,`raceId`,`cupId`),
+  ADD KEY `results_ibfk_3` (`raceId`),
+  ADD KEY `cupId` (`cupId`);
 
 --
 -- Indizes für die Tabelle `runners`
@@ -151,9 +212,21 @@ ALTER TABLE `categories`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT für Tabelle `clubs`
+--
+ALTER TABLE `clubs`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT für Tabelle `cups`
 --
 ALTER TABLE `cups`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `races`
+--
+ALTER TABLE `races`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
@@ -173,6 +246,13 @@ ALTER TABLE `cups_categories`
   ADD CONSTRAINT `cups_categories_ibfk_1` FOREIGN KEY (`cup_id`) REFERENCES `cups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints der Tabelle `cups_clubs`
+--
+ALTER TABLE `cups_clubs`
+  ADD CONSTRAINT `cups_clubs_ibfk_1` FOREIGN KEY (`cupId`) REFERENCES `cups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cups_clubs_ibfk_2` FOREIGN KEY (`clubId`) REFERENCES `clubs` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
 -- Constraints der Tabelle `races`
 --
 ALTER TABLE `races`
@@ -183,7 +263,8 @@ ALTER TABLE `races`
 --
 ALTER TABLE `results`
   ADD CONSTRAINT `results_ibfk_2` FOREIGN KEY (`runnerId`) REFERENCES `runners` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `results_ibfk_3` FOREIGN KEY (`raceId`) REFERENCES `races` (`solv_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `results_ibfk_3` FOREIGN KEY (`raceId`) REFERENCES `races` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `results_ibfk_4` FOREIGN KEY (`cupId`) REFERENCES `cups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints der Tabelle `runners`
